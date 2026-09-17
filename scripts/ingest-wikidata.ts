99256374e2c4b32806b40d0d0ecf2159f76a13de
Nora Shaipi <shaipin@lafayette.edu>
2026-09-17 11:58:33 -0400

    fixed error

--- scripts/ingest-wikidata.ts
+++ scripts/ingest-wikidata.ts
@@ -1,4 +1,7 @@
 import { fetchInfluencePage } from "../lib/wikidata";
 
-const rows = await fetchInfluencePage(0, 5);
-console.log(rows);
+async function main() {
+    const  rows = await fetchInfluencePage(0, 5);
+    console.log(rows);
+}
+main();
